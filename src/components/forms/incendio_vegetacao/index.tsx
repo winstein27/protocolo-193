f70c264e3a0aeb1733193bfa3fec5ca4149d2f03
Igor Muniz <muniz.igor@gmail.com>
2024-04-16 13:19:36 -0300

    Florestal passado para Formulario

--- src/components/forms/incendio_vegetacao/index.tsx
+++ src/components/forms/incendio_vegetacao/index.tsx
@@ -1,22 +1,28 @@
 import React, { useState, useEffect } from 'react';
-import { FormControl, RadioGroup, Radio, FormControlLabel, FormLabel, Grid, TextField, IconButton, Alert, AlertTitle } from '@mui/material';
-import { FileCopy as FileCopyIcon } from '@mui/icons-material';
+import { Grid } from '@mui/material';
+import { YesNoField, SwitchField, AlertMessage, Narrativa, CustomTextField } from '@site/src/components/main';
 
 const Formulario = () => {
-    const [pessoas, setPessoas] = useState(false);
-    const [animais, setAnimais] = useState(false);
-    const [residencias, setResidencias] = useState(false);
-    const [origem, setOrigem] = useState(false);
-    const [origemText, setOrigemText] = useState('');
-    const [causador, setCausador] = useState(false);
-    const [pessoaNoLocal, setpessoaNoLocal] = useState(false);
-    const [relato, setRelato] = useState('');
-    const [endereco, setEndereco] = useState('');
-    const [narrativa, setNarrativa] = useState('');
-    const [pontoReferencia, setPontoReferencia] = useState('');
-    const [irEstrada, setIrEstrada] = useState(false);
+    const [state, setState] = useState({
+        pessoas: false,
+        animais: false,
+        residencias: false,
+        origem: false,
+        origemText: '',
+        pessoaNoLocal: false,
+        relato: '',
+        endereco: '',
+        narrativa: '',
+        pontoReferencia: '',
+        irEstrada: false,
+        pontoReferenciaBoolean: false
+    });
 
+    const { pessoas, animais, residencias, origem, origemText, relato, endereco, narrativa, pontoReferencia, irEstrada, pontoReferenciaBoolean } = state;
 
+    const handleChange = (field, value) => {
+        setState(prevState => ({ ...prevState, [field]: value }));
+    };
 
     useEffect(() => {
         const text = `
@@ -30,168 +36,42 @@ Há pessoas no local: ${pessoas ? 'Sim' : 'Não'}
 Há animais no local: ${animais ? 'Sim' : 'Não'}
 Incêndio ameaça residências: ${residencias ? 'Sim' : 'Não'}
 Conseguiu identificar onde começou o incêndio: ${origem ? `Sim - ${origemText}` : 'Não'}
-Solicitante consegue ir para ponto de melhor localização?: ${irEstrada ? 'Sim' : 'Não'}
-    `;
-        setNarrativa(text);
-    }, [relato, endereco, pessoas, animais, residencias, origem, causador, pontoReferencia, origemText, irEstrada]);
+Solicitante orientado a ir para ponto de melhor localização?: ${irEstrada ? 'Sim' : 'Não'}
+  `;
+        setState(prevState => ({ ...prevState, narrativa: text }));
+    }, [relato, endereco, pessoas, animais, residencias, origem, origemText, irEstrada, pontoReferencia, pontoReferenciaBoolean]);
 
-    const handleCopy = () => {
-        navigator.clipboard.writeText(narrativa);
-    };
 
     return (
         <Grid container spacing={2}>
-            <Grid item xs={12} sx={{ mt: 4 }}>
-                <FormLabel component="legend">O que está acontecendo?</FormLabel>
-                <TextField
+            <CustomTextField label="O que está acontecendo?" name="relato" value={relato} onChange={handleChange} />
+            <CustomTextField label="Qual o endereço?" name="endereco" value={endereco} onChange={handleChange} />
+            <SwitchField label="Precisa de Ponto de Referência?" value={pontoReferenciaBoolean} onChange={(value) => handleChange("pontoReferenciaBoolean", value)} />
+            {pontoReferenciaBoolean && <CustomTextField label="Me dê um ponto de referência" name="pontoReferencia" value={pontoReferencia} onChange={handleChange} />}
 
-                    fullWidth
-                    value={relato} onChange={(e) => setRelato(e.target.value)}
-                />
-            </Grid>
-            <Grid item xs={12}>
-                <FormLabel component="legend">Qual o endereço?</FormLabel>
-                <TextField
-                    fullWidth
-                    value={endereco} onChange={(e) => setEndereco(e.target.value)}
-                />
-            </Grid>
-            <Grid item xs={12}>
-                <FormLabel component="legend">Me dê um ponto de referência</FormLabel>
-                <TextField
-                    fullWidth
-                    value={pontoReferencia} onChange={(e) => setPontoReferencia(e.target.value)}
-                />
-            </Grid>
-            <Grid item xs={12}>
-                <FormControl component="fieldset">
-                    <FormLabel component="legend">Há pessoas no local?</FormLabel>
-                    <RadioGroup row aria-label="pessoasBool" name="pessoasBool" value={pessoas.toString()} onChange={(e) => setPessoas(e.target.value === 'true')}>
-                        <FormControlLabel value="false" control={<Radio />} label="Não" />
-                        <FormControlLabel value="true" control={<Radio />} label="Sim" />
-                    </RadioGroup>
-                </FormControl>
-                {pessoas &&
-                    <Alert severity="warning">
-                        <AlertTitle>Informe ao Solicitante:</AlertTitle>
-                        "Tente todos em um local seguro"
-                    </Alert>
-                }
-            </Grid>
-            <Grid item xs={12}>
-                <FormControl component="fieldset">
-                    <FormLabel component="legend">Há animais no local?</FormLabel>
-                    <RadioGroup row aria-label="animaisBool" name="animaisBool" value={animais.toString()} onChange={(e) => setAnimais(e.target.value === 'true')}>
-                        <FormControlLabel value="false" control={<Radio />} label="Não" />
-                        <FormControlLabel value="true" control={<Radio />} label="Sim" />
-                    </RadioGroup>
-                </FormControl>
-                {animais &&
-                    <Alert severity="warning">
-                        <AlertTitle>Informe ao Solicitante:</AlertTitle>
-                        "Tente todos em um local seguro"
-                    </Alert>
-                }
-            </Grid>
-            <Grid item xs={12}>
-                <FormControl component="fieldset">
-                    <FormLabel component="legend">Incêndio está ameaçando residências?</FormLabel>
-                    <RadioGroup row aria-label="residenciasBool" name="residenciasBool" value={residencias.toString()} onChange={(e) => setResidencias(e.target.value === 'true')}>
-                        <FormControlLabel value="false" control={<Radio />} label="Não" />
-                        <FormControlLabel value="true" control={<Radio />} label="Sim" />
-                    </RadioGroup>
-                </FormControl>
-            </Grid>
-            <Grid item xs={12}>
-                <FormControl component="fieldset">
-                    <FormLabel component="legend">Conseguiu identificar onde começou o incêndio?</FormLabel>
-                    <RadioGroup row aria-label="origemBool" name="origemBool" value={origem.toString()} onChange={(e) => setOrigem(e.target.value === 'true')}>
-                        <FormControlLabel value="false" control={<Radio />} label="Não" />
-                        <FormControlLabel value="true" control={<Radio />} label="Sim" />
-                    </RadioGroup>
-                </FormControl>
-            </Grid>
+            <YesNoField label="Há pessoas no local?" value={pessoas} onChange={(value) => handleChange("pessoas", value)} />
+            {pessoas &&
+                <AlertMessage severity="warning" title="Informe ao Solicitante:">Por favor, peça pra todos se afastarem e ficarem em local seguro</AlertMessage>
+            }
+
+            <YesNoField label="Há animais no local?" value={animais} onChange={(value) => handleChange("animais", value)} />
+            {animais &&
+                <AlertMessage severity="warning" title="Informe ao Solicitante:">Por favor, tente colocar todos em um local seguro</AlertMessage>
+            }
+
+            <YesNoField label="Incêndio está ameaçando residências?" value={residencias} onChange={(value) => handleChange("residencias", value)} />
+
+            <YesNoField label="Conseguiu identificar onde começou o incêndio?" value={origem} onChange={(value) => handleChange("origem", value)} />
             {origem &&
-                <Grid item xs={12}>
-                    <TextField
-                        label="Onde começou?"
-                        value={origemText}
-                        onChange={(e) => setOrigemText(e.target.value)}
-                        fullWidth
-                    />
-                </Grid>}
-            {/* <Grid item xs={12}>
-                <FormControl component="fieldset">
-                    <FormLabel component="legend">Conseguiu identificar quem ou o quê causou o incêndio?</FormLabel>
-                    <RadioGroup row aria-label="causadorBool" name="causadorBool" value={causador.toString()} onChange={(e) => setCausador(e.target.value === 'true')}>
-                        <FormControlLabel value="false" control={<Radio />} label="Não" />
-                        <FormControlLabel value="true" control={<Radio />} label="Sim" />
-                    </RadioGroup>
-                </FormControl>
-            </Grid>
-            {causador &&
-                <Grid item xs={12}>
-                    <TextField
-                        label="Quem ou o quê causou?"
-                        fullWidth
-                    />
-                </Grid>}
-            {causador &&
-                <Grid item xs={12}>
-                    <FormControl component="fieldset">
-                        <FormLabel component="legend">Pessoa está no local??</FormLabel>
-                        <RadioGroup row aria-label="pessoaNoLocalBool" name="pessoaNoLocalBool" value={pessoaNoLocal.toString()} onChange={(e) => setpessoaNoLocal(e.target.value === 'true')}>
-                            <FormControlLabel value="false" control={<Radio />} label="Não" />
-                            <FormControlLabel value="true" control={<Radio />} label="Sim" />
-                        </RadioGroup>
-                    </FormControl>
-                </Grid>
-            } */}
-            <Grid item xs={12}>
-                <FormControl component="fieldset">
-                    <FormLabel component="legend">Consegue ir para a estrada ou outro
-                        lugar onde possamos encontrá-lo facilmente?</FormLabel>
-                    <RadioGroup row aria-label="irEstradaBool" name="irEstradaBool" value={irEstrada.toString()} onChange={(e) => setIrEstrada(e.target.value === 'true')}>
-                        <FormControlLabel value="false" control={<Radio />} label="Não" />
-                        <FormControlLabel value="true" control={<Radio />} label="Sim" />
-                    </RadioGroup>
-                </FormControl>
-                {irEstrada &&
-                    <Alert severity="warning">
-                        <AlertTitle>Informe ao Solicitante:</AlertTitle>
-                        "OK, leve o celular contigo. Vamos enviar uma equipe o quanto antes"
-                    </Alert>
-                }
-                {!irEstrada &&
-                    <Alert severity="warning">
-                        <AlertTitle>Informe ao Solicitante:</AlertTitle>
-                        "OK, Não permaneça em local seguro e não tente combater o incêndio, vamos
-                        enviar uma equipe para o quanto antes"
-                    </Alert>
-                }
-            </Grid>
+                <CustomTextField label="Onde começou?" name="origemText" value={origemText} onChange={handleChange} />
+            }
 
-            <Grid item xs={12} sx={{
-                mt: 10,
-            }}>
-                <FormLabel component="legend">Copie o texto abaixo e insira na Narrativa</FormLabel>
-                <TextField
-                    sx={{
-                        backgroundColor: 'rgba(0, 255, 0, 0.1)',
-                    }}
-                    multiline
-                    fullWidth
-                    value={narrativa}
-                    InputProps={{
-                        disabled: true,
-                        endAdornment: (
-                            <IconButton onClick={handleCopy} aria-label="copy">
-                                <FileCopyIcon />
-                            </IconButton>
-                        ),
-                    }}
-                />
-            </Grid>
+            <YesNoField label="Consegue ir para a estrada ou outro lugar onde possamos encontrá-lo facilmente?" value={irEstrada} onChange={(value) => handleChange("irEstrada", value)} />
+            <AlertMessage severity="warning" title="Informe ao Solicitante:">
+                {irEstrada ? "OK, leve o celular contigo. Vamos enviar uma equipe o quanto antes" : "OK, permaneça em local seguro e não tente combater o incêndio. Vamos enviar uma equipe o quanto antes"}
+            </AlertMessage>
+
+            <Narrativa value={narrativa} />
         </Grid>
     );
 };
